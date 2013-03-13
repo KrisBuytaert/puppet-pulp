0986ced8ffe86c8c2d56e1555f360f0942846169
Martin Jackson <martin@uncommonsense-uk.com>
2013-03-13 09:16:01 +0000

    Added server configuration variables.

--- manifests/server.pp
+++ manifests/server.pp
@@ -15,12 +15,26 @@
 #  * Add documentation.
 #
 class pulp::server (
-  $qpid_server = 'localhost'
+  $mail_enabled     = 'false',
+  $mail_host        = 'localhost',
+  $mail_host_port   = '25',
+  $mail_from        = undef,
+  $mongodb_host     = 'localhost',
+  $mongodb_port     = '27017',
+  $qpid_server      = 'localhost',
+  $qpid_port        = '5672',
+  $pulp_server_name = false
 ) {
   $packagelist = ['pulp-puppet-plugins', 'pulp-rpm-plugins', 'pulp-selinux', 'pulp-server']
 
   package { $packagelist:
-      ensure => installed,
+    ensure => installed,
+  }
+  file { '/etc/pulp/server.conf':
+    owner => 'root',
+    group => 'root',
+    mode => 0644,
+    content => template('pulp/server.conf.erb'),
   }
 
 }
