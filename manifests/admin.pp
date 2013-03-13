845fc50d80b90578b5ec8f841faf5e4342c9ae8e
Martin Jackson <martin@uncommonsense-uk.com>
2013-03-13 16:06:40 +0000

    Added pulp-admin module for pulp-admin tool.

---
+++ manifests/admin.pp
@@ -0,0 +1,19 @@
+# class pulp::admin
+class pulp::admin (
+$pulp_server_host = 'localhost.localdomain',
+$pulp_server_port = '443'
+) inherits pulp::params {
+
+  $packagelist = ['pulp-admin-client', 'pulp-puppet-admin-extensions','pulp-rpm-admin-extensions']
+  package { $packagelist:
+    ensure  => 'installed',
+  }
+  file { '/etc/pulp/admin/admin.conf':
+    ensure  => 'file',
+    content => template('pulp/admin.conf.erb'),
+    mode    => '0644',
+    owner   => 'root',
+    group   => 'root',
+    require => Package[$packagelist],
+  }
+}
